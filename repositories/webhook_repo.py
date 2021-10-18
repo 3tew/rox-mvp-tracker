41e710e21e219967d0ed07a22422d9c3b3d7ab59
deda-official <87776847+deda-official@users.noreply.github.com>
2021-10-18 10:54:52 +0700

    add loudspeaker icon to refreshing

--- repositories/webhook_repo.py
+++ repositories/webhook_repo.py
@@ -52,7 +52,7 @@ def send_message_webhook(case, options):
 def refreshing_message_case(boss_data, isAbyss, strTime, data):
     data['username'] = 'ROX - MVP Announcer'
     data['embeds'][0]['color'] = 65504  # Blue
-    data['embeds'][0]['title'] = \
+    data['embeds'][0]['title'] = '📢 ' + \
         ('Abyss ' + boss_data['fullName']) if isAbyss else (boss_data['fullName']) + \
         (" `Development`" if config.IS_DEVELOPMENT else "")
     data['embeds'][0]['description'] = \
