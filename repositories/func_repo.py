0c9e7b86414070373629a94861e2283b9aedf163
deda-official <87776847+deda-official@users.noreply.github.com>
2021-10-24 18:20:47 +0700

    add delay

--- repositories/func_repo.py
+++ repositories/func_repo.py
@@ -41,7 +41,7 @@ def mouse_click_mvp_tab():
         x = (config.EMULATOR_X + config.MVP_BOUNDING_BOX_CENTER_X)
         y = (config.EMULATOR_Y + config.MVP_BOUNDING_BOX_CENTER_Y)
         pyautogui.click(x=x, y=y)
-        time.sleep(0.2)  # Delay 200 milliseconds
+        time.sleep(0.25)  # Delay 250 milliseconds
 
 
 def mouse_click_mini_tab():
@@ -49,7 +49,7 @@ def mouse_click_mini_tab():
         x = (config.EMULATOR_X + config.MINI_BOUNDING_BOX_CENTER_X)
         y = (config.EMULATOR_Y + config.MINI_BOUNDING_BOX_CENTER_Y)
         pyautogui.click(x=x, y=y)
-        time.sleep(0.2)  # Delay 200 milliseconds
+        time.sleep(0.25)  # Delay 250 milliseconds
 
 
 def mouse_draging():
@@ -65,6 +65,7 @@ def mouse_draging():
         pyautogui.dragTo(x=x2, y=y2 + 5, button='left',  # Calibrating
                         duration=0.125, mouseDownUp=False)
         pyautogui.mouseUp(button='left')
+        time.sleep(0.1)  # Delay 100 milliseconds
 
 
 def alert(title, message, kind='info'):
